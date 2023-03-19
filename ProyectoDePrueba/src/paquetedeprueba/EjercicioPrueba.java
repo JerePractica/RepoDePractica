b6d93b6bb139350d8f57236f84e9aacc0cc26319
Jeremias Rivelli <jeremaia21211@hotmail.com>
2023-03-19 12:11:54 -0300

    Cambios hechos y primer ejercicio guia

--- ProyectoDePrueba/src/paquetedeprueba/EjercicioPrueba.java
+++ ProyectoDePrueba/src/paquetedeprueba/EjercicioPrueba.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquetedeprueba;
 
-/**
- *
- * @author Jerem
- */
-public class Ejercio1 {
-    
+import java.util.Scanner;
+
+/*Escribir un programa que pida dos números enteros por teclado y calcule la suma de los dos. 
+El programa deberá después mostrar el resultado de la suma */
+public class EjercicioPrueba {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("Ingrese dos números");
+        int num1 = leer.nextInt();
+        int num2 = leer.nextInt();
+        int suma = num1 + num2;
+
+        System.out.println("La suma de los números es: " + suma);
+    }
 }
